01c1bd4d0ed2162c80fb5e7cb2ce3e5c69c235fd
wanetty <eduard.gm@gmail.com>
2018-04-10 21:49:12 +0200

    SHIT.

--- P4/webserver.go
+++ P4/webserver.go
@@ -32,8 +32,6 @@ func main() {
 
 router := mux.NewRouter().StrictSlash(true)
 router.HandleFunc("/", Index)
-//router.HandleFunc("/endpoint/{param}", endpointFunc)
-//router.HandleFunc("/endpoint2/{param}", endpointFunc2JSONInput)
 router.HandleFunc("/carrentalnew",carrentalFuncnew)
 router.HandleFunc("/carrentalread",carrentalFuncread)
 
@@ -124,34 +122,4 @@ func writeToFile(w http.ResponseWriter, make int, model int, nofdays int, nofuni
     writer.Flush()
     file.Close()
 }
-
-
-/*func endpointFunc(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    param := vars["param"]
-    res := ResponseMessage{Field1: "Text1", Field2: param}
-    json.NewEncoder(w).Encode(res)
-    
-}
-
-func endpointFunc2JSONInput(w http.ResponseWriter, r *http.Request) {
-    var requestMessage RequestMessage
-    body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-    if err != nil {
-        panic(err)
-    }
-    if err := r.Body.Close(); err != nil {
-        panic(err)
-    }
-    if err := json.Unmarshal(body, &requestMessage); err != nil {
-        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-        w.WriteHeader(422) // unprocessable entity
-        if err := json.NewEncoder(w).Encode(err); err != nil {
-            panic(err)
-        }
-    } else {
-        fmt.Fprintln(w, "Successfully received request with Field1 =", requestMessage.Field1)
-        fmt.Println(r.FormValue("queryparam1"))
-    }
-}*/
 
